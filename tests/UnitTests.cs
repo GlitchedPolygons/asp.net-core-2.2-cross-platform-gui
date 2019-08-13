fd8740bcf2a272c5a6b880c20514e692f2bf2eee
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-08-13 15:22:05 +0200

    Added unit tests project (xUnit) - ready to be filled with your own tests!

---
+++ tests/UnitTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace CrossPlatformGUI.UnitTests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void ExampleTest1()
+        {
+            Assert.True(1 + 1 == 2);
+        }
+    }
+}
